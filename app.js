651a8bbe63900b3629aeb63aa957f2676d97e09c
kurikulumsman2cikarangbarat-hub <kurikulum.sman2cikarangbarat@gmail.com>
2025-12-31 17:37:01 +0700

    Update and rename script.js to app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>CBT Online</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div id="login">
+  <h2>CBT Online</h2>
+  <input id="token" placeholder="Token Ujian">
+  <input id="nama" placeholder="Nama Peserta">
+  <button onclick="login()">Masuk</button>
+</div>
+
+<div id="exam" hidden>
+  <h3 id="soal"></h3>
+  <div id="opsi"></div>
+  <button onclick="submit()">Kirim</button>
+</div>
+
+<pre id="debug"></pre>
+
+<script src="app.js"></script>
+</body>
+</html>
